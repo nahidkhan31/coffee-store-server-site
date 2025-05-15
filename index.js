12115806b0a69cbb24b44f67d60e2871eb5ccf89
Nahid Khan <nahidkhanniloy31@gmail.com>
2025-05-16 01:45:44 +0600

    added code

--- index.js
+++ index.js
@@ -25,6 +25,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coffeeCollection = client.db("coffeeDB").collection("coffees");
+
+    app.get("/coffees", async (req, res) => {
+      //   const cursor = coffeeCollection.find();
+      //   const result = await cursor.toArray();
+      const result = await coffeeCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/coffees", async (req, res) => {
+      const newCoffee = req.body;
+      console.log(newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
